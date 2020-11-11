fbc34da319babe9188532a127caeab869489126e
Aleksey Moiseev <moiseev.003@bk.ru>
2020-11-11 21:41:59 +0300

    Второе занятие по OOP разбор приватных, публичных методов, атрибутов instans, репрозентация str как образец написали класс Стэка

---
+++ Python Lesson/Lesson 12 OOP/study1.py
@@ -0,0 +1,30 @@
+"""
+Stack
+"""
+
+
+class Stack:
+    """Этот класс описание Стэка"""
+    def __init__(self):
+        self._container = []  # "_ позволяет обозначить метод как приватный"
+
+    def __str__(self):
+        return str(self._container)
+
+    def pop(self):
+        value = self._container[1]
+        del self._container[-1]
+        return value
+
+    def push(self, obj):
+        self._container.append(obj)
+
+    def is_empty(self):
+        return len(self._container) == 0
+
+    def top(self):
+        return self._container[-1]
+
+
+if __name__ == '__main__':
+    s = Stack()
